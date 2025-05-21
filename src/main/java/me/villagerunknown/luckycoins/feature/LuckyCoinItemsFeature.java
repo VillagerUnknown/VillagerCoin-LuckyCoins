5240059bc1e9e2aa79aa767195c62ca8008a4748
villagerunknown <VillagerUnknown@proton.me>
2025-05-20 22:45:53 -0400

    updates from VC

--- src/main/java/me/villagerunknown/luckycoins/feature/LuckyCoinItemsFeature.java
+++ src/main/java/me/villagerunknown/luckycoins/feature/LuckyCoinItemsFeature.java
@@ -6,7 +6,6 @@ import me.villagerunknown.villagercoin.feature.InventoryEffectCoinFeature;
 public class LuckyCoinItemsFeature {
 	
 	public static void execute() {
-		InventoryEffectCoinFeature.execute();
 		new LuckyCoinItems();
 	}
 	
